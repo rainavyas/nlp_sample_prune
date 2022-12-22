0e0f17d882d09fc9a8cecf156d990564783a4c9b
Vyas Raina <vr313@gpu-e-8.data.cluster>
2022-12-22 12:49:06 +0000

    speedup for stopword pruning

--- src/data/prune.py
+++ src/data/prune.py
@@ -4,6 +4,7 @@ from nltk.tokenize import word_tokenize
 from tqdm import tqdm
 import os
 import json
+from collections import Counter
 
 class Pruner():
     @classmethod
@@ -28,8 +29,10 @@ class Pruner():
     @staticmethod
     def remove_stopword(ds):
         nltk.download('stopwords')
+        stop_words = stopwords.words()
+        stopwords_dict = Counter(stop_words)
         new_ds = []
         for sample in tqdm(ds):
-            text = ' '.join([w for w in word_tokenize(sample['text']) if not w in stopwords.words()])
+            text = ' '.join([w for w in word_tokenize(sample['text']) if not w in stopwords_dict])
             new_ds.append({'text':text, 'label':sample['label']})
         return new_ds
